6bc9edf39ee4745f8ede292bba362cc646b9f008
skoshpaev <sts.ko@mail.ru>
2021-01-21 18:33:06 +0300

    first_commit docker + symfony has been added

---
+++ install.sh
@@ -0,0 +1,15 @@
+cp docker/docker-compose-test.yml.example docker-compose-test.yml
+cp docker/docker-compose.yml.example docker-compose.yml
+cp docker/var.env.example docker/var.env
+cp docker/var.env.example docker/var_test.env
+cp .env .env.local
+
+docker network create helastel
+docker network create helastel_test
+alias dd='docker-compose'
+alias console='docker-compose exec fpm bin/console'
+dd up -d
+
+dd exec fpm composer install
+console -V
+sh test.sh
---
+++ test.sh
@@ -0,0 +1,18 @@
+docker network create helastel_test
+cp docker/docker-compose-test.yml.example docker-compose-test.yml
+cp docker/var.env.example docker/var_test.env
+cp .env.test .env.test.local
+
+alias dd_test='docker-compose -f docker-compose-test.yml'
+dd_test up -d
+
+FILE=bin/phpunit
+if test -f "$FILE"; then
+    echo "$FILE exists. No further actions."
+else
+    echo "$FILE does not exist. Adding a dependency"
+    dd_test exec fpm_test composer require --dev symfony/phpunit-bridge
+fi
+
+dd_test exec fpm_test bin/phpunit --bootstrap=tests/bootstrap.php
+dd_test down
